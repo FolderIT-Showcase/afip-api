bf247d77a5014d7e88f69469e4625ee8ffbf7070
Miguel Bustos <mbustos@folderit.net>
2017-05-04 17:03:13 -0300

    Agregada proteccion contra DDoS

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ var express = require('express'),
 	mongoose = require('mongoose'),
 	config = require('./config'),
 	favicon = require('serve-favicon'),
+	toobusy = require('toobusy-js'),
 	logger = require('tracer').colorConsole(config.loggerFormat);
 
 global.environment = config.environment;
 global.loggerFormat = config.loggerFormat;
 global.tokenSecret = config.tokenSecret;
 global.appRoot = require('app-root-path').path;
+
+toobusy.maxLag(200);
+toobusy.interval(1000);
 
 logger.info("Entorno:", global.environment);
 
@@ -23,13 +27,19 @@ db.on('error', (err) => {
 db.once('open', function () {
 	logger.info("Connection to DB established");
 
+	// Control de sobrecarga
+	app.use(function (req, res, next) {
+		if (toobusy()) res.status(503).send("El servicio se encuentra sobrecargado. Por favor, intente nuevamente.");
+		else next();
+	});
+
 	// Contenido estático
 	app.use(favicon(path.join(__dirname, 'public', 'assets', 'img', 'favicon.ico')));
 	app.use(express.static(path.join(__dirname, 'public')));
 	app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')));
 
-	app.listen(process.env.PORT || 3000, function () {
-		logger.info('AFIP API listening on port', (process.env.PORT || 3000));
+	app.listen(process.env.PORT || 80, function () {
+		logger.info('AFIP API listening on port', (process.env.PORT || 80));
 
 		// Inicializacion de modelos de la base de datos
 		require('./models');
