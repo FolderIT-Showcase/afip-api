722ef347ac33689b124a2bdf56f3b2b802a7dd54
Miguel Bustos <mbustos@folderit.net>
2017-05-16 13:47:53 -0300

    Agregadas funciones de generacion de CSR en SignHelper

--- helpers/SignHelper.js
+++ helpers/SignHelper.js
@@ -1,8 +1,8 @@
-var openssl = require('openssl-wrapper').exec,
-    logger = require('tracer').colorConsole(global.loggerFormat);
-
-// Expose methods.
-exports.sign = sign;
+var pify = require("pify"),
+    logger = require('tracer').colorConsole(global.loggerFormat),
+    os = require('os'),
+    openssl = pify(require('openssl-wrapper').exec),
+    csrgen = pify(require('csr-gen'));
 
 /**
  * Sign a file.
@@ -19,13 +19,13 @@ function sign(options) {
         options = options || {};
 
         if (!options.content)
-            reject('Invalid content.');
+            reject({ message: 'Invalid content.' });
 
         if (!options.key)
-            reject('Invalid key.');
+            reject({ message: 'Invalid key.' });
 
         if (!options.cert)
-            reject('Invalid certificate.');
+            reject({ message: 'Invalid certificate.' });
 
         var content = new Buffer(options.content);
 
@@ -40,15 +40,60 @@ function sign(options) {
             ssl.passin = "pass:" + options.password;
         }
 
-        openssl('smime.sign', content, ssl, function (err, buffer) {
-            if (err) {
-                logger.error(err);
-                reject({ message: "No se pudo validar el certificado del cliente. Contáctese con el Administrador del sistema de Facturación Electrónica." });
-            } else {
-                var enc = buffer.toString('base64');
-
-                resolve(enc);
-            }
+        openssl('smime.sign', content, ssl).then((buffer) => {
+            var enc = buffer.toString('base64');
+            resolve(enc);
+        }).catch((err) => {
+            logger.error(err);
+            reject({
+                message: "No se pudo validar el certificado del cliente. Contáctese con el Administrador del sistema de Facturación Electrónica."
+            });
         });
     });
+}
+
+function gencsr(options) {
+    return new Promise((resolve, reject) => {
+        options = options || {};
+
+        if (!options.domain)
+            reject({ message: 'Falta completar el Alias/Código del cliente.' });
+
+        if (!options.email)
+            reject({ message: 'Falta completar el Email del cliente.' });
+
+        if (!options.company)
+            reject({ message: 'Falta completar la Razón Social del cliente.' });
+
+        if (!options.cuit)
+            reject({ message: 'Falta completar el CUIT del cliente.' });
+
+        var ssl = {
+            read: true,
+            destroy: true,
+            country: 'AR',
+            state: '',
+            city: '',
+            division: '',
+            password: '',
+            company: options.company,
+            email: options.email,
+            serialNumber: 'CUIT ' + options.cuit
+        };
+
+        csrgen(options.domain, ssl).then((keys) => {
+            resolve(keys);
+        }).catch((err) => {
+            logger.error(err);
+            reject({
+                message: "Ocurrió un error al intentar generar las llaves del certificado."
+            });
+        });
+    });
+}
+
+// Expose methods.
+module.exports = {
+    sign: sign,
+    gencsr: gencsr
 }
