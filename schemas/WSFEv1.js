15f6de7bf9cf896e66b3b9271417ed098707089d
Miguel Bustos <mbustos@folderit.net>
2017-07-13 13:53:12 -0300

    Agregado modulo de esquemas de endpoints de servicios AFIP

---
+++ schemas/WSFEv1.js
@@ -0,0 +1,104 @@
+module.exports = {
+    FECAESolicitar: {
+        ImpNeto: {
+            type: "number",
+            default: 0
+        },
+        ImpConc: {
+            type: "number",
+            default: 0
+        },
+        ImpOpEx: {
+            type: "number",
+            default: 0
+        },
+        ImpTrib: {
+            type: "number",
+            default: 0
+        },
+        ImpIva: {
+            type: "number",
+            default: 0
+        },
+        IdIVA: {
+            type: "number"
+        },
+        DocNro: {
+            type: "string",
+            required: true
+        },
+        PtoVta: {
+            type: "number",
+            required: true
+        },
+        DocTipo: {
+            type: "number",
+            default: 80
+        },
+        CbteNro: {
+            type: "number",
+            required: true
+        },
+        CbteFch: {
+            type: "string",
+            isDate: true
+        },
+        Concepto: {
+            type: "number",
+            default: 2
+        },
+        CantReg: {
+            type: "number",
+            default: 1
+        },
+        CbteTipo: {
+            type: "number",
+            required: true
+        },
+        MonId: {
+            type: "string",
+            default: "PES"
+        },
+        MonCotiz: {
+            type: "number",
+            default: 1
+        },
+        Tributos: {
+            type: "array",
+            items: [{
+                type: "object",
+                properties: {
+                    Id: {
+                        type: "number",
+                        required: true
+                    },
+                    Desc: {
+                        type: "string"
+                    },
+                    BaseImp: {
+                        type: "number",
+                        default: 0
+                    },
+                    Alic: {
+                        type: "number",
+                        required: true
+                    },
+                    Importe: {
+                        type: "number",
+                        default: 0
+                    }
+                }
+            }]
+        }
+    },
+    FECompUltimoAutorizado: {
+        PtoVta: {
+            type: "number",
+            required: true
+        },
+        CbteTipo: {
+            type: "number",
+            required: true
+        }
+    }
+}
